de29b944e48ba7b0ac5cff86ab7521ad583c54f3
Sanket <sanket0249@gmail.com>
2018-04-16 13:05:56 +0530

    Update harvest.sol
    0.0.2

--- harvest.sol
+++ harvest.sol
@@ -1,3 +1,9 @@
+//*************** Time Locked based smart contract*********
+//      1. contract creator is admin
+//      2. any funds sent to contract address stays locked for n number of days(in this case 10 days)
+//      3. Funds can be withdrawn after time limit expires 
+//      4. funds are sent to the admin/creator of the smart contract
+
 pragma solidity ^0.4.11;
 
 contract timeDelay {
@@ -21,13 +27,20 @@ contract timeDelay {
         _;
     }
     
+    //receive funds from any address
     function() public payable {
         
     }
     
+    //return funds only to admin
     function withdraw() public onlyAdmin {
         require (block.timestamp > harvest);
         msg.sender.transfer(address(this).balance);
+    }
+    
+    // incase anything comes up admin is able to kill contract and retrive funds
+    function kill() public onlyAdmin {
+        selfdestruct(admin);
     }
     
 }
